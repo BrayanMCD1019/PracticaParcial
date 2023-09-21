381197736719a8b8b590829e3836242a4d476745
JormanNoriega <jormandavidnoriegadavila@gmail.com>
2023-09-20 23:51:18 -0500

    agrege atributos

---
+++ Entity/LiquidacionCuotaModeradora.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class LiquidacionCuotaModeradora
+    {
+        private int numLiquidacion {  get; set; }
+        private int idPaciente { get; set;}
+        private string tipoAfiliacion { get; set; }
+        private double salarioDevengado { get; set; }
+        private double valorServicio { get; set; }
+
+    }
+}
