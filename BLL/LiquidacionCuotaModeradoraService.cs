4bddd2ee34e730f0a054adeb7606e11a1badc565
BrayanMCD1019 <brayanjr1019@gmail.com>
2023-09-24 18:49:03 -0500

    mostrar liquidaciones

--- BLL/LiquidacionCuotaModeradoraService.cs
+++ BLL/LiquidacionCuotaModeradoraService.cs
@@ -48,6 +48,69 @@ namespace BLL
             return liquidacionCuotaModeradoraList;
         }
 
+        public void MostrarLiquidaciones()
+        {
+            try
+            {
+                liquidacionCuotaModeradoraList = liquidacionCuotaModeradoraRepository.ConsultarTodos();
+
+                if (liquidacionCuotaModeradoraList.Count == 0)
+                {
+                    Console.WriteLine("No hay liquidaciones registradas.");
+                    return;
+                }
+
+                Console.WriteLine("Lista de liquidaciones realizadas:");
+                Console.WriteLine("-----------------------------------");
+
+                foreach (var liquidacion in liquidacionCuotaModeradoraList)
+                {
+                    double tarifaCuotaModeradora = 0;
+                    double cuotaModeradora = 0;
+
+                    tarifaCuotaModeradora = liquidacion.tipoAfiliacion.Equals("Subsidiado", StringComparison.OrdinalIgnoreCase) ? 0.5 : CalcularTarifaAfiliadoContributivo(liquidacion.salarioDevengado);
+
+                    cuotaModeradora = Math.Min(liquidacion.valorServicio * tarifaCuotaModeradora, 200000);
+
+                    Console.WriteLine($"Identificación del Paciente: {liquidacion.idPaciente}");
+                    Console.WriteLine($"Tipo de Afiliación: {liquidacion.tipoAfiliacion}");
+                    Console.WriteLine($"Salario Devengado por el Paciente: {liquidacion.salarioDevengado}");
+                    Console.WriteLine($"Valor del Servicio de Hospitalización Prestado: {liquidacion.valorServicio}");
+                    Console.WriteLine($"Tarifa Aplicada: {tarifaCuotaModeradora}");
+                    Console.WriteLine($"Valor Liquidado Real de la Cuota Moderadora: {cuotaModeradora}");
+                    Console.WriteLine(cuotaModeradora == 200000 ? "Se aplicó el tope máximo." : "No se aplicó el tope máximo.");
+                    Console.WriteLine($"Valor de la Cuota Moderadora: {cuotaModeradora}");
+                    Console.WriteLine("-----------------------------------");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al mostrar las liquidaciones: {ex.Message}");
+            }
+        }
+
+        private double CalcularTarifaAfiliadoContributivo(double salarioPaciente)
+        {
+            double salarioMinimo = 1160000;
+            double tarifaCuotaModeradora = 0;
+
+            double factor = salarioPaciente / salarioMinimo;
+
+            switch (factor)
+            {
+                case var _ when factor < 2:
+                    tarifaCuotaModeradora = 0.15;
+                    break;
+                case var _ when factor < 5:
+                    tarifaCuotaModeradora = 0.2;
+                    break;
+                default:
+                    tarifaCuotaModeradora = 0.25;
+                    break;
+            }
+
+            return tarifaCuotaModeradora;
+        }
 
         /*c) consulta que permita filtrar por tipo de afiliación totalizando cantidad de liquidaciones
         realizadas, liquidaciones del régimen subsidiado y liquidaciones de régimen contributivo,*/
